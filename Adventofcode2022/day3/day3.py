3e2787b38d033978057a4dde96a49b6f27cda7e1
TheoLindh <lindh.theo73@gmail.com>
2023-01-09 00:50:04 +0100

    cleaned up a bit, i think i understand myself now

--- Adventofcode2022/day3/day3.py
+++ Adventofcode2022/day3/day3.py
@@ -1,4 +1,3 @@
-from collections import Counter
 f = open("C:/Users/Theo/Documents/Adventofcode2022/advent-of-code-2022/Adventofcode2022/day3/inputday3.txt", "r")
 packs = f.readlines()
 sum = 0
@@ -8,14 +7,11 @@ for comp in packs:
     splitted2 = comp[length//2:]
     res = str("")
     for i in splitted:
-        if i in splitted2 and not i in res:
-            res += i
-    
-    a = ord(res)
-
-    if res.isupper() == True:
-        a -= 38
-    else:
-        a -= 96
-    sum += a
+        if i in splitted2 :
+            a = ord(i)
+            if i.isupper() == True:
+                a -= 38
+            else:
+                a -= 96
+    sum += a    
 print(sum)
