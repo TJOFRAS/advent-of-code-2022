7efbc3a8d3dea51bbee15070383323157ed8b9e8
TheoLindh <lindh.theo73@gmail.com>
2023-01-10 21:10:03 +0100

    Made the first part of day 4

--- Adventofcode2022/day4/day4.py
+++ Adventofcode2022/day4/day4.py
@@ -1,13 +1,30 @@
 def isFullyContained(elf1, elf2):
-    return elf1[0] < elf2[0] and elf1[1] > elf2[1]
-    
-def convertElfsToInt
-f = open("C:/Users/Theo/Documents/Adventofcode2022/advent-of-code-2022/Adventofcode2022/day4/inputday4.txt", "r")
-fileContent = f.readlines()
+    return (elf1[0] <= elf2[0] and elf1[1] >= elf2[1]) or (elf1[0] >= elf2[0] and elf1[1] <= elf2[1])
 
-for workingPairs in fileContent:
+def importinput(path):
+    input = open(path, "r")
+    inputlist = input.readlines()
+    return inputlist
 
-    workingPairs = workingPairs.replace("\n","")
-    workingPairs = workingPairs.split(",")
-    workingPairs = [workingPairs[0].split("-") , workingPairs[1].split("-")]
-    print(workingPairs)
+def stringTolist(string):
+    stringsplitted = string.split(',')
+    string1 = stringsplitted[0]
+    string2 = stringsplitted[1]
+    string1 = string1.split('-')
+    string2 = string2.split('-')
+    cleanList = [int(string1[0]), int(string1[1])],[ int(string2[0]), int(string2[1])]
+    return cleanList
+
+
+def main():
+    amountOfFullyContainedElfpairs = 0
+    inputlist = importinput(path)
+    for elfpair in inputlist:
+        elfpair = stringTolist(elfpair)
+        if isFullyContained(elfpair[0], elfpair[1]) == True:
+            amountOfFullyContainedElfpairs += 1
+    return print(amountOfFullyContainedElfpairs)
+
+path = "C:/Users/Theo/Documents/Adventofcode2022/advent-of-code-2022/Adventofcode2022/day4/inputday4.txt"
+main()
+
